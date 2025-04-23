cf3734d88ce3059dd813a15fc46c5c4212f84903
Tran Duc Tri <mega19012003@gmail.com>
2025-04-23 22:07:04 +0700

    cart

---
+++ WebBookStore/Models/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace WebBookStore.Models
+{
+    public class CartItem
+    {
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; } // Giá tại thời điểm thêm vào giỏ (tránh thay đổi sau này)
+        public decimal TotalPrice => UnitPrice * Quantity;
+    }
+}
